1618de4287ae24a6939fd8f8a811cec4f081169c
Naoki maruyama <s23c1135vq@s.chibakoudai.jp>
2024-11-30 01:04:06 +0900

    課題1

--- money_rate_test.bash
+++ money_rate_test.bash
@@ -3,14 +3,14 @@
 # 入力引数のチェック
 if [[ $# -eq 0 ]]; then
     echo "エラー: 円の金額を引数として指定してください。" >&2
-    echo "使用例: ./test.bash 1000" >&2
+    echo "使用例: ./money_rate_test.bash 1000" >&2
     exit 1
 fi
 
 # 入力値が数値かどうかを確認
 if ! [[ $1 =~ ^[0-9]+$ ]]; then
     echo "エラー: 引数には数字を入力してください。" >&2
-    echo "使用例: ./test.bash 1000" >&2
+    echo "使用例:  ./money_rate_test.bash 1000" >&2
     exit 1
 fi
 
